04ad7edb9a2ee73fcf8c0b98e5e80d5da6f54e35
hab <mohab.khalid.mahmoud@gmail.com>
2023-07-03 23:47:56 +0300

    try font

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'package:flaticon/flaticon.dart';
 import 'package:flutter/material.dart';
-import 'package:tasks/ui/verbs/verbs_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: VerbsScreen(),
+    home: City(),
     debugShowCheckedModeBanner: false,
   ));
+}
+
+class City extends StatelessWidget {
+  const City({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              FlatIcons.star_solid,
+              size: 50,
+              color: Colors.yellow,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
